74025ef738b14652a293fe28e6ac5d17f613fff0
meng <meng@qq.com>
2018-10-15 13:22:51 +0800

    server-customer消费者

---
+++ server-customer/src/main/java/com/jk/controller/TestController.java
@@ -0,0 +1,9 @@
+package com.jk.controller;
+
+import com.jk.service.test.TestService;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController implements TestService {
+
+}
