857fadfa85fe92cf845325ccecbf59bfbb5a3b66
meng <meng@qq.com>
2018-10-15 12:39:12 +0800

    server-eureka注册中心

---
+++ server-eureka/src/main/java/com/jk/ServerEurekaApplication.java
@@ -0,0 +1,14 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ServerEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerEurekaApplication.class, args);
+    }
+}
